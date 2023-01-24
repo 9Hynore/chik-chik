4b66593dccd00e8f20de22880c51913a09e957a5
Hynore <68786826+9Hynore@users.noreply.github.com>
2023-01-24 10:25:51 +0600

    adaptive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+const API_URL = 'https://elegant-shrub-maraca.glitch.me/';
 
+/*
+GET /api - получить список услуг
+GET /api?service={n} - получить список барберов
+GET /api?spec={n} - получить список месяца работы барбера
+GET /api?spec={n}&month={n} - получить список дней работы барбера
+GET /api?spec={n}&month={n}&day={n} - получить список свободных часов барбера
+POST /api/order - оформить заказ
+*/
 
 const addPreload = (elem) =>{
     elem.classList.add('preload')
@@ -16,16 +25,49 @@ const startSlider = () => {
     let activeSlide = 1;
     let position = 0;
 
+    const checkSlider = () =>{
+        if ((activeSlide + 2 === sliderItems.length && document.documentElement.offsetWidth > 560) || activeSlide === sliderItems.length){
+            btnNextSlide.style.display = 'none';
+        }else{btnNextSlide.style.display = '';}
+
+        if(activeSlide === 1){
+        btnPrevSlide.style.display = 'none';
+        }else{btnPrevSlide.style.display = '';}
+    }
     const nextSlide = () => {
-        sliderItems[activeSlide].classList.remove('.slider__item_activ')
+        sliderItems[activeSlide]?.classList.remove('slider__item_active');
+        position = -sliderItems[0].clientWidth * activeSlide;
+        
+        sliderList.style.transform = `translateX(${position}px)`;
+        activeSlide += 1;
+        sliderItems[activeSlide]?.classList.add('slider__item_active');
+        checkSlider()
 }
     const prevSlide = () =>{
+        sliderItems[activeSlide]?.classList.remove('slider__item_active');
+        position = -sliderItems[0].clientWidth * (activeSlide - 2);
         
+        sliderList.style.transform = `translateX(${position}px)`;
+        activeSlide -= 1;
+        sliderItems[activeSlide]?.classList.add('slider__item_active');
+        checkSlider()
     }
     
 
     btnPrevSlide.addEventListener('click', prevSlide);
     btnNextSlide.addEventListener('click', nextSlide);
+
+    window.addEventListener('resize', () => {
+        if( activeSlide + 2 > sliderItems.length &&
+             document.documentElement.offsetWidth > 560){
+                activeSlide = sliderItems.length - 2;
+                sliderItems[activeSlide]?.classList.add('slider__irem_active')
+        }
+
+        position = -sliderItems[0].clientWidth * (activeSlide - 1)
+        sliderList.style.transform = `translateX(${position}px)`;
+        checkSlider()
+    })
     
 }
 const initSlider = () =>{
@@ -41,6 +83,16 @@ const initSlider = () =>{
 
         startSlider()
     })
+};
+
+const initService = () => {
+    const priceList = document.querySelector('.price__list');
+    priceList.textContent = ''; 
+}
+
+const init = () =>{
+    initSlider();
+    initService()
 }
 
 window.addEventListener('DOMContentLoaded',initSlider);
